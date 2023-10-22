e71f756e24959b74394021cfab00cbf789b2ff9f
lucas.sproule <lucas.sproule.42@gmail.com>
2023-10-21 22:16:55 -0400

    add subdir lib sh*t Command

--- src/Command/CommandHelp.cpp
+++ src/Command/CommandHelp.cpp
@@ -6,9 +6,10 @@ namespace Command{
 
   int help() {
     std::cout << "usage: " << ENDL  
-      "init: initializes your project" << ENDL
-      "run : builds and runs your project" <<ENDL
-      "flags: adds a flag to your project" << ENDL;
+      "Commands:" << ENDL
+      "   init: initializes your project" << ENDL
+      "   run : builds and runs your project" <<ENDL
+      "   flags: adds a flag to your project" << ENDL;
     return 1;
   }
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,6 @@
 #include <memory>
 #include <string>
 
-enum Language { CPP, C };
-
-typedef struct version {
-  std::string type;
-  std::string version;
-} version;
-
-namespace CVersion {
-  version c89 = {"CMAKE_C_STANDARD", "89"};
-  version c90 = {"CMAKE_C_STANDARD", "90"};
-  version c99 = {"CMAKE_C_STANDARD", "99"};
-  version c11 = {"CMAKE_C_STANDARD", "11"};
-  version c17 = {"CMAKE_C_STANDARD", "17"};
-  version c18 = {"CMAKE_C_STANDARD", "18"};
-  version c2x = {"CMAKE_C_STANDARD", "2x"};
-}; // namespace CVersion
-
-enum ProjectType { 
-  LIBRARY,
-  EXECUTABLE 
-};
-
-void generateBasicProject(std::string project_name) {}
-
-
 int main(int argc, char **argv) {
   if (argc < 2) {
     Command::help();
