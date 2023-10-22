8e8e0f9c25fe3192bb87eba7b208276f7eb500a0
ryan <ryan.mason814@gmail.com>
2023-10-21 20:29:09 -0700

    BATMAN

--- src/Command/CommandHelp.cpp
+++ src/Command/CommandHelp.cpp
@@ -6,8 +6,8 @@ namespace Command {
   int help() {
     std::cout << "usage: idk do what you want" << ENDL  
       "Commands:" << ENDL
-      "\tinit:  initializes your project"     << ENDL
-      "\trun:   builds and runs your project" << ENDL
+      "\t i | init:  initializes your project"     << ENDL
+      "\t r | run:   builds and runs your project" << ENDL
       "\tflags: adds a flag to your project"  << ENDL
       "\thelp:  print help"  << ENDL;
     return 1;
--- src/main.cpp
+++ src/main.cpp
@@ -3,29 +3,68 @@
 #include <memory>
 #include <string>
 
-int main(int argc, char **argv) {
-  if (argc < 2) {
+enum cmd_options
+{
+  i = 0,
+  init = 1,
+  r = 2,
+  run = 3,
+  flags = 4,
+  help = 5
+
+};
+
+int main(int argc, char **argv)
+{
+  if (argc < 2)
+  {
     Command::help();
     std::cout << "needs more args"
               << "\n";
     return 1;
   }
+
   std::string command = argv[1];
-  //LUCAS MAKE SURE YOU INITIALIZE YOUR FUCKING STRUCT YOU TWAT
+  // LUCAS MAKE SURE YOU INITIALIZE YOUR FUCKING STRUCT YOU TWAT
   std::shared_ptr<Command::Context> ctx = std::make_shared<Command::Context>();
-  if (command == "init") {
-    Command::init(ctx);
+
+  static std::map<std::string, cmd_options> const table = {
+      {"i", cmd_options::i},
+      {"init", cmd_options::init},
+      {"r", cmd_options::r},
+      {"run", cmd_options::run},
+      {"flags", cmd_options::flags},
+      {"help", cmd_options::help},
+  };
+  auto selection = cmd_options::help; // default
+  auto it = table.find(command);
+  if (it != table.end())
+  {
+    selection = it->second;
   }
-  if (command == "run") {
+
+  switch (selection)
+  {
+  case 0: // i / init
+  case 1:
+    Command::init(ctx);
+    break;
+
+  case 2: // r / run
+  case 3:
     Command::loadPackageToml(ctx);
     Command::run(ctx);
-  }
-  if (command == "flags"){
+    break;
+
+  case 4: // flags
     Command::loadPackageToml(ctx);
     Command::addFlag(ctx, argv[2]);
-  }
-  if (command == "help"){
+    break;
+
+  case 5: // help
+  default:
     Command::help();
+    break;
   }
   return 0;
 }
