5c2e41487ef497e9115ba6c9bfd2e22a1961e481
Jon-Michael Hartway <jhartway99@gmail.com>
2023-10-24 14:17:09 -0400

    merge

--- src/Command/CommandHelp.cpp
+++ src/Command/CommandHelp.cpp
@@ -2,12 +2,17 @@
 #include "Command.hpp"
 
 namespace Command {
-
   bool help() {
-    std::cout << "usage: idk do what you want" << ENDL  
+    std::cout << "usage: cmaker <sub-command>" << ENDL  
       "Commands:" << ENDL
-      "\t i | init:  initializes your project"     << ENDL
+      "\t i | init [<package-name>]" << ENDL
+      "\t          [-y | --skip-init]" << ENDL
+      "\t          [-n | --name example-name] " << ENDL
+      "\t          [-l | --language cpp/c]:" << ENDL
+      "\t          initializes your project" << ENDL
       "\t r | run:   builds and runs your project" << ENDL
+      "\t add <name> <github-url> <branch-tag> : adds a dependency to your project" << ENDL
+      "\t ftp:  deletes the entire project" << ENDL
       "\t flags: adds a flag to your project"  << ENDL
       "\t help:  print help"  << ENDL;
     return 1;
