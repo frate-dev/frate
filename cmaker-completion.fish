c01eb1db7c9a4377be3f6a0e7a56363465238e32
sakheli <morisi1999@gmail.com>
2023-11-05 00:02:10 +0400

    adding tab completion

---
+++ cmaker-completion.fish
@@ -0,0 +1,27 @@
+function __fish_cmaker_needs_command
+    set -l cmd (commandline -opc)
+    if test (count $cmd) -eq 1
+        return 0
+    end
+    return 1
+end
+
+function __fish_cmaker_using_command
+    set -l cmd (commandline -opc)
+    set -e cmd[1]
+    if contains -- $argv[1] $cmd
+        return 0
+    end
+    return 1
+end
+
+function __fish_cmaker_needs_subcommand
+    set -l cmd (commandline -opc)
+    if test (count $cmd) -eq 2
+        return 0
+    end
+    return 1
+end
+
+complete -c cmaker -n '__fish_cmaker_needs_command' -a 'init run watch add ftp help' -f
+complete -c cmaker -n '__fish_cmaker_using_command add' -a 'dep lib flags' -f
