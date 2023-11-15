88ff92036f57da5ade48a2c82969e6f2edc28bde
Jon-Michael Hartway <jhartway99@gmail.com>
2023-11-14 22:01:41 -0500

    fixed help command

--- src/Command/CommandHelp.cpp
+++ src/Command/CommandHelp.cpp
@@ -7,7 +7,8 @@
 #include <termcolor/termcolor.hpp>
 
 namespace Command {
-  bool Interface::help() {
+  //We're keeping this for our long lost friend ryan aka fired docs guy
+  bool oldHelp() {
     if ((static_cast<std::string>(std::getenv("TERM")) == "xterm-kitty")){
       struct winsize w = {500, 500, 0, 0};
       ioctl(0, TIOCGWINSZ, &w);
@@ -38,6 +39,47 @@ namespace Command {
       // SACRED CODE BLOCK ENDS HERE
     std::cout << termcolor::red << "This is a pre-alpha version of cmaker, please report any bugs here: " << termcolor::reset
       << " https://github.com/cmaker-dev/cmaker/issues" << ENDL;
+
     return 1;
+  }
+  bool Interface::help() {
+    if((static_cast<std::string>(std::getenv("TERM")) == "xterm-kitty")){
+      struct winsize w = {500, 500, 0, 0};
+      ioctl(0, TIOCGWINSZ, &w);
+      int success = Utils::hSystem("kitty +kitten icat --align left ~/Downloads/logo.png");
+      if(success != 0){
+        std::cout << "Error printing logo" << std::endl;
+        return false;
+      }
+    }
+
+    std::cout << R"(
+usage: cmaker <sub-command>
+Commands:
+i | init [<package-name>]
+         [-y | --skip-init]
+         [-n | --name example-name] 
+         [-l | --language cpp/c]
+         : initializes your project
+
+run : builds and runs your project
+watch : watches builds and runs your project on changes
+clean
+          [-c | --cache] : cleans the cache
+          : cleans the build directory
+
+add  [dep, lib, flags] 
+          [-l | --latest] : latest version of the package
+          : add library, dependency or flags to your project
+
+modes [Debug, Test, Release] : Add and Remove compilation modes/definitions
+search <query> : search for a package
+server : manages remote build servers for your projects
+update [index] : updates the index of packages
+ftp : deletes the entire project (F*ck this project)
+flags : adds a flag to your project - probably doesn't work right now
+help : print help
+)" << termcolor::red << "This is a pre-alpha version of cmaker, please report any bugs here: " << termcolor::reset;
+    return false;
   }
 }
