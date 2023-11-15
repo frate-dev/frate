6090fed511c283e0ef7bf9895c1c4f34af06593a
Jon-Michael Hartway <jhartway99@gmail.com>
2023-11-14 21:29:07 -0500

    Removed some of those pesky system commands #20

--- src/Command/CommandRun.cpp
+++ src/Command/CommandRun.cpp
@@ -1,51 +1,53 @@
+#include <filesystem>
 #include <iostream>
 #include "Command.hpp"
 #include <string>
 #include "../Utils/General.hpp"
 
 namespace Command {
-  bool Interface::run(){
-    std::cout << "Running project: " << pro->project_name << std::endl;
+using std::filesystem::create_directories;
 
-    const std::string work_dir_cmd = "cd " + pro->project_path.string();
-    const std::string mkdir_build_dir = work_dir_cmd + ";mkdir -p build";
-    const std::string cmake_cmd = work_dir_cmd + ";cmake .";
-    const std::string make_cmd = work_dir_cmd + ";make";
-    const std::string run_cmd = work_dir_cmd + ";./build/" + pro->project_name;
+bool Interface::run() {
+  std::cout << "Running project: " << pro->project_name << std::endl;
 
-    /*
-     * Creating build directory and building project
-     */
-    int success = Utils::hSystem(mkdir_build_dir);
+  const std::string work_dir_cmd = "cd " + pro->project_path.string();
+  const std::string cmake_cmd = work_dir_cmd + ";cmake .";
+  const std::string make_cmd = work_dir_cmd + ";make";
+  const std::string run_cmd = work_dir_cmd + ";./build/" + pro->project_name;
 
-    if (success != 0) {
-      std::cout << "Error creating build directory" << std::endl;
-      return false;
-    }
-
-    success = Utils::hSystem(cmake_cmd);
-    if (success != 0) {
-      std::cout << "Error running cmake" << std::endl;
-      return false;
-    }
-
-    success = Utils::hSystem(make_cmd);
-    if (success != 0) {
-      std::cout << "Error running make" << std::endl;
-      return false;
-    }
-
-    /*
-     * Running project
-     */
-
-    success = Utils::hSystem(run_cmd);
-    if (success != 0) {
-      std::cout << "Error running project" << std::endl;
-      return false;
-    }
-
-    return true;
+  /*
+   * Creating build directory and building project
+   */
+  try{
+    create_directories(pro->project_path / "build");
+  }catch(std::exception e){
+    std::cout << "Failed to create build directory" << std::endl;
+    return false;
   }
-}
 
+
+  int success = Utils::hSystem(cmake_cmd);
+  if (success != 0) {
+    std::cout << "Error running cmake" << std::endl;
+    return false;
+  }
+
+  success = Utils::hSystem(make_cmd);
+  if (success != 0) {
+    std::cout << "Error running make" << std::endl;
+    return false;
+  }
+
+  /*
+   * Running project
+   */
+
+  success = Utils::hSystem(run_cmd);
+  if (success != 0) {
+    std::cout << "Error running project" << std::endl;
+    return false;
+  }
+
+  return true;
+  }
+  } // namespace Command
--- src/Command/IndexCache.cpp
+++ src/Command/IndexCache.cpp
@@ -4,35 +4,44 @@
 
 namespace Command{
   using nlohmann::json;
-  std::string indexFileName = std::string(std::getenv("HOME")) + "/.local/cmaker/index.json";
-  std::string indexUrl = "https://github.com/cmaker-dev/index/releases/latest/download/index.json";
+  using std::cout;
+  using std::ifstream;
+  using std::ofstream;
+  using std::string;
+  using std::filesystem::create_directories;
+  std::string indexFileName =
+    std::string(std::getenv("HOME")) + "/.local/cmaker/index.json";
+  std::string indexUrl =
+    "https://github.com/cmaker-dev/index/releases/latest/download/index.json";
   /*
    * If index is not in path then we create one
    * returns fetched index
    */
-  json fetchIndex(){
+  json fetchIndex() {
     json index;
-    if(std::filesystem::exists(indexFileName)){
-      std::ifstream indexFile;
+    if (std::filesystem::exists(indexFileName)) {
+
+      ifstream indexFile;
       indexFile = std::ifstream(indexFileName);
       index = json::parse(indexFile);
-      std::cout << "index file exists" <<ENDL;
-    }else{
-      std::ofstream indexFile; 
-      std::cout << "Creating a new index file" << ENDL;
-      if(Utils::hSystem("mkdir -p ~/.local/cmaker") != 0){
+      std::cout << "index file exists" << ENDL;
+
+    } else {
+      ofstream indexFile;
+      cout << "Creating a new index file" << ENDL;
+
+      try {
+        create_directories(string(std::getenv("HOME")) + "/.local/cmaker");
+      }catch(std::exception e){
         std::cout << "Failed to create index file" << ENDL;
         exit(-1);
       }
-      if(Utils::hSystem("touch ~/.local/cmaker/index.json") != 0){
-        std::cout << "Failed to create index file" << ENDL;
-        exit(-1);
-      }
+
       index = Utils::fetchJson(indexUrl);
-      std::cout << "god dee index" << ENDL;
       indexFile = std::ofstream(indexFileName);
       indexFile << index.dump(2);
       std::cout << "index file created" <<ENDL;
+
     }
 
     return index;
@@ -44,19 +53,27 @@ namespace Command{
    */
   void updateIndex(){
     std::ofstream indexFile;
+
     try{
+
       //Checks if the index file exists
       indexFile = std::ofstream(indexFileName);
+
     }catch(std::exception e){
+
       std::cout << "Creating a new index file" << ENDL;
-      if(Utils::hSystem("mkdir -p ~/.local/cmaker") != 0){
+
+      try{
+
+        create_directories(string(std::getenv("HOME")) + "/.local/cmaker");
+
+      }catch(std::exception e){
+
         std::cout << "Failed to create index file" << ENDL;
         exit(-1);
+
       }
-      if(Utils::hSystem("touch ~/.local/cmaker/index.json") != 0){
-        std::cout << "Failed to create index file" << ENDL;
-        exit(-1);
-      }
+
       std::ifstream indexFile(indexFileName);
     }
 
@@ -64,15 +81,20 @@ namespace Command{
     indexFile << data.dump(2);
   }
   std::pair<bool,Package> getExactPackage(std::string package_name){
+
     json index = fetchIndex();
+
     for(auto package : index){
+
       if(package["name"] == package_name){
+
         Package p;
         p.name = package["name"];
         p.versions = package["versions"];
         p.git = package["git"];
         p.description = package["description"];
         return std::pair<bool,Package>{true,p};
+
       }
     }
     return std::pair<bool,Package>{false,Package{}};
