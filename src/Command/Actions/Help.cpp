3847b32bf16ce9fd9a5b27c7880a25d4a1be57b1
lucas sprouole <lucas.sproule.42@gmail.com>
2023-12-08 00:05:24 -0500

    commented out image stuff

--- src/Command/Actions/Help.cpp
+++ src/Command/Actions/Help.cpp
@@ -5,15 +5,15 @@
 namespace Frate::Command::Help {
   //We're keeping this for our long lost friend ryan aka fired docs guy
   bool run(Interface* inter) {
-    if((static_cast<std::string>(std::getenv("TERM")) == "xterm-kitty")){
-      struct winsize w = {500, 500, 0, 0};
-      ioctl(0, TIOCGWINSZ, &w);
-      int success = Utils::hSystem("kitty +kitten icat --align left ~/Downloads/logo.png");
-      if(success != 0){
-        std::cout << "Error printing logo" << std::endl;
-        return false;
-      }
-    }
+   // if((static_cast<std::string>(std::getenv("TERM")) == "xterm-kitty")){
+   //   struct winsize w = {500, 500, 0, 0};
+   //   ioctl(0, TIOCGWINSZ, &w);
+   //   int success = Utils::hSystem("kitty +kitten icat --align left ~/Downloads/logo.png");
+   //   if(success != 0){
+   //     std::cout << "Error printing logo" << std::endl;
+   //     return false;
+   //   }
+   // }
     inter->getHelpString("frate",inter->commands);
     return true;
   }
